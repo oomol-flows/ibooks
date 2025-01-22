c05c87b0a7c80b52c8be8d03c0dddf098396d4cd
TaoZeyu <i@taozeyu.com>
2025-01-22 10:58:53 +0800

    feat(blocks): update styles logic

--- tasks/generate-html/__init__.py
+++ tasks/generate-html/__init__.py
@@ -11,7 +11,7 @@ def main(params: dict):
   buffer.write("<html>\n")
 
   _wrtie_head(buffer, title, styles)
-  _write_body(buffer, params["highlights"])
+  _write_body(buffer, title, params["highlights"])
 
   buffer.write("</html>")
   return { "html": buffer.getvalue() }
@@ -31,22 +31,39 @@ def _wrtie_head(buffer: StringIO, title: str | None, styles: str | None):
     buffer.write("</style>")
   buffer.write("</head>\n")
 
-def _write_body(buffer: StringIO, highlights: list[dict]):
-  buffer.write("<body>")
+def _write_body(buffer: StringIO, title: str | None, highlights: list[dict]):
+  a_icon = "https://api.iconify.design/academicons:academia-square.svg?color=%23888888"
+  chat_icon = "https://api.iconify.design/fluent-mdl2:message.svg?color=%23888888"
+  buffer.write('<body><div class="content">\n')
+
+  if title is not None:
+    buffer.write("<h1>")
+    buffer.write(escape(title))
+    buffer.write("</h1>\n")
+
   for highlight in _search_highlights(highlights):
     if highlight.selected is None and highlight.note is None:
       continue
-    buffer.write("<div>")
     if highlight.selected is not None:
-      buffer.write("<blockquote>")
+      buffer.write('<div class="row source">')
+      buffer.write(f'<img class="icon" src="{a_icon}"/>')
+      buffer.write('<p class="text">')
       buffer.write(escape(highlight.selected))
-      buffer.write("</blockquote>")
+      buffer.write("</p></div>\n")
+
     if highlight.note is not None:
-      buffer.write("<p>")
+      buffer.write('<div class="row note">')
+      buffer.write(f'<img class="icon" src="{chat_icon}"/>')
+      buffer.write('<p class="text">')
       buffer.write(escape(highlight.note))
-      buffer.write("</p>")
-    buffer.write("</div>")
-  buffer.write("</body>\n")
+      buffer.write("</p></div>\n")
+
+    buffer.write('<div class="row by-date">')
+    buffer.write('<p class="text page">6</p>\n')
+    buffer.write('<p class="text dateAndAuthor">12月 19, 17:24, Tom</p>')
+    buffer.write("</div>\n")
+
+  buffer.write("</div></body>\n")
 
 def _search_highlights(highlights: list[dict]):
   for highlight in highlights:
